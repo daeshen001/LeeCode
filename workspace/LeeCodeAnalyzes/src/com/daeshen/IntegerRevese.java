8ffbf9a166784d7fafdc7f68615f2db3f028d5e2
daeshen001 <1497354161@qq.com>
2017-10-20 16:05:42 +0800

    modify reverse

--- workspace/LeeCodeAnalyzes/src/com/daeshen/IntegerRevese.java
+++ workspace/LeeCodeAnalyzes/src/com/daeshen/IntegerRevese.java
@@ -4,7 +4,12 @@ public class IntegerRevese {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+  System.out.println(reverse(-123));
   }
 
+  
+  public static  int reverse(Integer x){
+    int a= Integer.reverse(x);
+    return a;
+  }
 }
