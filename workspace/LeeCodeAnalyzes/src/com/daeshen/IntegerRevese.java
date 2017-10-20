028f25ef52675995de955c20fd0210687381d694
daeshen001 <1497354161@qq.com>
2017-10-20 16:09:06 +0800

    modify IntegerReverse

--- workspace/LeeCodeAnalyzes/src/com/daeshen/IntegerRevese.java
+++ workspace/LeeCodeAnalyzes/src/com/daeshen/IntegerRevese.java
@@ -4,12 +4,24 @@ public class IntegerRevese {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-  System.out.println(reverse(-123));
+    //System.out.println(reverse(Integer.MIN_VALUE));
+    //System.out.println(Integer.MAX_VALUE);
+  System.out.println(reverse(1234));
   }
 
   
-  public static  int reverse(Integer x){
-    int a= Integer.reverse(x);
-    return a;
+  public static  int reverse(int x){
+    if(x==Integer.MIN_VALUE)
+      return Integer.MIN_VALUE;
+    int temp = Math.abs(x);
+    int res =0;
+    while(temp!=0){
+      if(res>(Integer.MAX_VALUE-temp%10)/10)//运用巧妙的边界问题
+        return x>0?Integer.MAX_VALUE:Integer.MIN_VALUE;
+      res=res*10+temp%10;
+      System.out.println(res+"+"+temp);
+      temp/=10;
+            }
+    return x>0?res:-res;
   }
 }
